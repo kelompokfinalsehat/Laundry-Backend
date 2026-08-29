3f942f4e1e321f44a05a30ea569f1a157280ade4
Damargilang-09 <damargilangardhina@gmail.com>
2026-08-29 20:45:01 +0700

    feat(address): tambah endpoint preview-location untuk titik awal peta Leaflet

--- src/features/region/region.controllers.ts
+++ src/features/region/region.controllers.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { RegionService } from "./region.services";
 import { validate } from "../../validations/validate";
 import { RegionValidation } from "./region.validations";
+import { GeocodingUtil } from "../../utils/geocoding.util";
 
 export class RegionController {
   static async getProvinces(req: Request, res: Response) {
@@ -14,22 +15,32 @@ export class RegionController {
     const { params } = validate(RegionValidation.GET_CITIES, {
       params: req.params,
     });
-    const data = await RegionService.getCities({params});
+    const data = await RegionService.getCities({ params });
     res.json({ success: true, data });
   }
 
   static async getDistricts(req: Request, res: Response) {
-    const { params } = validate(RegionValidation.GET_DISTRICTS,{
-      params:req.params
-    }) 
-    const data = await RegionService.getDistricts({params});
+    const { params } = validate(RegionValidation.GET_DISTRICTS, {
+      params: req.params,
+    });
+    const data = await RegionService.getDistricts({ params });
     res.json({ success: true, data });
   }
-  static async getSubDistricts(req:Request,res:Response){
-    const {params} = validate(RegionValidation.GET_SUB_DISTRICTS,{
-      params:req.params
-    })
-    const data = await RegionService.getSubDistrict({params})
+
+  static async getSubDistricts(req: Request, res: Response) {
+    const { params } = validate(RegionValidation.GET_SUB_DISTRICTS, {
+      params: req.params,
+    });
+    const data = await RegionService.getSubDistrict({ params });
+    res.json({ success: true, data });
+  }
+
+  static async previewLocation(req: Request, res: Response) {
+    const { body } = validate(RegionValidation.PREVIEW_LOCATION, {
+      body: req.body,
+    });
+
+    const data = await RegionService.previewLocation({ body });
     res.json({ success: true, data });
   }
 }
