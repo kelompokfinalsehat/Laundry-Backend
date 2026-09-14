225a9e029a0a0f98cc81cde3f03131463ae983bc
Muhammad Rifqi Maulana <rfqimaulana888@gmail.com>
2026-09-14 14:10:57 +0700

    fix: penyelesaian komplain sekaligus menyelesaikan orderan customer

--- src/features/complaint/complaint.repository.ts
+++ src/features/complaint/complaint.repository.ts
@@ -1,31 +1,55 @@
-import { ComplaintStatus, Prisma } from "../../../generated/prisma";
+import {
+  ComplaintStatus,
+  CustomerStatus,
+  Prisma,
+} from "../../../generated/prisma";
 import { prisma } from "../../configs/prisma-client.config";
 import { PaginationHelper } from "../../helpers/pagination.helper";
 import { ComplaintHelper } from "./complaint.helper";
 import { ComplaintQuery, DecideDTOParams } from "./complaint.type";
 
 export class ComplaintRepository {
-    static async findAll(query: ComplaintQuery, outletId?: string){
-        const {page, pageSize, skip, take} = PaginationHelper.paginate(query)
-        const where = ComplaintHelper.defineComplaintQuery(query, outletId)
-        const [complaints, totalItems] = await prisma.$transaction([
-            prisma.complaint.findMany({where, skip, take, include: ComplaintHelper.listInclude, orderBy: {[query.sortBy]: query.sortOrder}}),
-            prisma.complaint.count({where})
-        ])
-        return {
-            data: complaints,
-            meta: PaginationHelper.meta(page, pageSize, totalItems)
-        }
-    }
-    static async findById(id: string, outletId?: string){
-        return prisma.complaint.findFirst({where: {id, ...(outletId && {order: {outletId}})}, include: ComplaintHelper.detailInclude})
-    }
-    static async decide({id, handledBy, responseNote}:DecideDTOParams){
-        return prisma.complaint.update({where: {id}, data: {
-            status: ComplaintStatus.APPROVED,
-            handledBy,
-            responseNote,
-            decidedAt: new Date()
-        }})
-    }
+  static async findAll(query: ComplaintQuery, outletId?: string) {
+    const { page, pageSize, skip, take } = PaginationHelper.paginate(query);
+    const where = ComplaintHelper.defineComplaintQuery(query, outletId);
+    const [complaints, totalItems] = await prisma.$transaction([
+      prisma.complaint.findMany({
+        where,
+        skip,
+        take,
+        include: ComplaintHelper.listInclude,
+        orderBy: { [query.sortBy]: query.sortOrder },
+      }),
+      prisma.complaint.count({ where }),
+    ]);
+    return {
+      data: complaints,
+      meta: PaginationHelper.meta(page, pageSize, totalItems),
+    };
+  }
+  static async findById(id: string, outletId?: string) {
+    return prisma.complaint.findFirst({
+      where: { id, ...(outletId && { order: { outletId } }) },
+      include: ComplaintHelper.detailInclude,
+    });
+  }
+  static async decide({ id, handledBy, responseNote }: DecideDTOParams) {
+    const now = new Date()
+    return await prisma.$transaction(async (tx) => {
+      const complaint = await tx.complaint.update({
+        where: { id },
+        data: {
+          status: ComplaintStatus.APPROVED,
+          handledBy,
+          responseNote,
+          decidedAt: now,
+        },
+      });
+      await tx.order.update({
+        where: { id: complaint.orderId },
+        data: { customerStatus: CustomerStatus.RECEIVED_BY_CUSTOMER, completedAt: now },
+      });
+      return tx.complaint.findFirst({where: {id: complaint.id}})
+    });
+  }
 }
